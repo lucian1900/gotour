abae959d64ea06160552f3d44c0b7fba4b38365f
Lucian Brănescu-Mihăilă <lucian1900@users.noreply.github.com>
2012-07-06 20:59:34 +0100

    Same function. Doesn't work.

--- tree.go
+++ tree.go
@@ -13,6 +13,7 @@ func Walk(t *tree.Tree, ch chan int) {
     if t.Left != nil {
         go Walk(t.Left, ch)
     }
+
     if t.Right != nil {
         go Walk(t.Right, ch)
     }
@@ -20,14 +21,28 @@ func Walk(t *tree.Tree, ch chan int) {
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+    vals := t1.Value == t2.Value
+
+    if vals == false {
+        return false
+    }
+
+    lefts := Same(t1.Left, t2.Left)
+    rights := Same(t1.Right, t2.Right)
+
+    return t1 == t2 && lefts && rights
+}
 
 func main() {
-    t := tree.New(1)
     ch := make(chan int)
-    go Walk(t, ch)
+    go Walk(tree.New(1), ch)
 
     for i := 1; i < 10; i++ {
         fmt.Printf("%d ", <- ch)
     }
+    fmt.Println("")
+
+    fmt.Println(Same(tree.New(1), tree.New(1)))
+    fmt.Println(Same(tree.New(1), tree.New(2)))
 }
